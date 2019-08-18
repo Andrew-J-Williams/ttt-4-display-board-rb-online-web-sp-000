5d99f847615f47f8badc58babb29423d2a09129a
Andrew Williams <1williamsaj@gmail.com>
2019-08-18 14:25:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,5 +4,9 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
